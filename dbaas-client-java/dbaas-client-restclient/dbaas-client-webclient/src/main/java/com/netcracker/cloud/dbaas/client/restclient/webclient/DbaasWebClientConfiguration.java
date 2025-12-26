8daa141ff34cf6c923459cd07cbf8f27d5621227
Artem Kuleshov <artem.kuleshov@netcracker.com>
2025-12-26 10:58:44 +0500

    fix: override global Jackson configuration to ignore unknown fields (#143)

--- dbaas-client-java/dbaas-client-restclient/dbaas-client-webclient/src/main/java/com/netcracker/cloud/dbaas/client/restclient/webclient/DbaasWebClientConfiguration.java
+++ dbaas-client-java/dbaas-client-restclient/dbaas-client-webclient/src/main/java/com/netcracker/cloud/dbaas/client/restclient/webclient/DbaasWebClientConfiguration.java
@@ -1,11 +1,13 @@
 package com.netcracker.cloud.dbaas.client.restclient.webclient;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.netcracker.cloud.restclient.MicroserviceRestClient;
 import com.netcracker.cloud.restclient.webclient.MicroserviceWebClient;
 import com.netcracker.cloud.smartclient.config.annotation.EnableFrameworkWebClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -18,9 +20,17 @@ public class DbaasWebClientConfiguration {
 
     @Bean("dbaasRestClient")
     public MicroserviceRestClient dbaasRestClient(@Qualifier("m2mWebClient") WebClient webClient) {
-        WebClient webClientWithoutMetrics = webClient.mutate().filters(new DisableHttpTraceFilterConsumer()).build();
+        WebClient customizedWebClient = webClient.mutate()
+                .filters(new DisableHttpTraceFilterConsumer())
+                .codecs(clientCodecsConfigurer -> clientCodecsConfigurer.defaultCodecs()
+                        .configureDefaultCodec(o -> {
+                            if (o instanceof Jackson2JsonDecoder decoder) {
+                                decoder.getObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                            }
+                        }))
+                .build();
 
-        return new MicroserviceWebClient(webClientWithoutMetrics);
+        return new MicroserviceWebClient(customizedWebClient);
     }
 
     // If sleuth enabled, it tries to get db health from http filters. But dataSource can be not initialized yet.
